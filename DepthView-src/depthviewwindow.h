24cf4b60471ab05582c62aff95296a09015ebd50
chipgw <gw.chip.gw@gmail.com>
2011-09-13 18:53:53 +0000

    deleted whitespace

--- DepthView-src/depthviewwindow.h
+++ DepthView-src/depthviewwindow.h
@@ -37,15 +37,11 @@ private slots:
     void on_actionOpen_triggered();
     void on_actionExit_triggered();
     void on_actionAbout_Qt_triggered();
-                                             void on_actionSave_As_triggered();
-                                                                                      void on_actionNo_Mirror_triggered();
-                                                                                                                               void on_actionMirror_Left_triggered();
-                                                                                                                                                                        void on_actionMirror_Right_triggered();
-                                                                                                                                                                                                                 void on_actionMirror_Both_triggered();
-                                                                                                                                                                                                                                                          void on_actionFit_triggered();
-                                                                                                                                                                                                                                                                                                   void on_actionzoom100_triggered();
-                                                                                                                                                                                                                                                                                                                                            void on_actionzoom50_triggered();
-                                                                                                                                                                                                                                                                                                                                                                                     void on_actionzoom200_triggered();
-                                           };
+    void on_actionSave_As_triggered();
+    void on_actionNo_Mirror_triggered();
+    void on_actionMirror_Left_triggered();
+    void on_actionMirror_Right_triggered();
+    void on_actionMirror_Both_triggered();
+};
 
 #endif // DEPTHVIEWWINDOW_H
