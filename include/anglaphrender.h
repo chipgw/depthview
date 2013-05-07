b5f10cc95198996f20b8559ca66ffab740f51ebc
chipgw <gw.chip.gw@gmail.com>
2013-05-07 09:48:43 -0500

    drawAnglaph now internally supports custom eye colors.

--- include/anglaphrender.h
+++ include/anglaphrender.h
@@ -3,7 +3,7 @@
 
 #include <QImage>
 
-QImage drawAnglaph(const QImage &imgL, const QImage &imgR, int panX, int panY, int finalwidth = 0, int finalheight = 0, float zoom = 0.0f, float colormult = 0.0f);
+QImage drawAnglaph(const QImage &imgL, const QImage &imgR, int panX, int panY, int finalwidth = 0, int finalheight = 0, float zoom = 0.0f, float colormult = 0.0f, QRgb colorL = qRgb(255, 0, 0), QRgb colorR = qRgb(0, 255, 255));
 
 
 #endif // ANGLAPHRENDER_H
--- src/anglaphrender.cpp
+++ src/anglaphrender.cpp
@@ -2,7 +2,7 @@
 #include <QTime>
 #include <QDebug>
 
-QImage drawAnglaph(const QImage &imgL, const QImage &imgR, int panX, int panY, int finalwidth, int finalheight, float zoom, float colormult){
+QImage drawAnglaph(const QImage &imgL, const QImage &imgR, int panX, int panY, int finalwidth, int finalheight, float zoom, float colormult, QRgb colorL, QRgb colorR){
     QTime starttime = QTime::currentTime();
 
     if(finalwidth <= 0 || finalheight <= 0){
@@ -14,8 +14,8 @@ QImage drawAnglaph(const QImage &imgL, const QImage &imgR, int panX, int panY, i
         zoom = qMin((float)finalwidth / (float)imgL.width(), (float)finalheight / (float)imgL.height());
     }
 
-    panX += (finalwidth*0.5f  - imgL.width()  * zoom * 0.5f);
-    panY += (finalheight*0.5f - imgL.height() * zoom * 0.5f);
+    panX += (finalwidth  * 0.5f - imgL.width()  * zoom * 0.5f);
+    panY += (finalheight * 0.5f - imgL.height() * zoom * 0.5f);
 
     QImage final(finalwidth,finalheight,QImage::Format_RGB32);
 
@@ -23,27 +23,41 @@ QImage drawAnglaph(const QImage &imgL, const QImage &imgR, int panX, int panY, i
     QRgb *lineL;
     QRgb *lineR;
 
-    for(int y=0;y<final.height();y++){
+    float redL   = qRed(colorL)   / 255.0f;
+    float redR   = qRed(colorR)   / 255.0f;
+    float greenL = qGreen(colorL) / 255.0f;
+    float greenR = qGreen(colorR) / 255.0f;
+    float blueL  = qBlue(colorL)  / 255.0f;
+    float blueR  = qBlue(colorR)  / 255.0f;
+
+    float greymult = 1.0f - colormult;
+
+    for(int y = 0; y < final.height(); y++){
         line = (QRgb *)final.scanLine(y);
-        int cy = (y-panY)/zoom;
+        int cy = (y - panY) / zoom;
         if(cy >= 0 && cy < imgL.height()){
             lineL = (QRgb *)imgL.constScanLine(cy);
             lineR = (QRgb *)imgR.constScanLine(cy);
-            for(int x=0;x<final.width();x++){
-                int cx = (x-panX)/zoom;
+            for(int x = 0; x < final.width(); x++){
+                int cx = (x - panX) / zoom;
                 if(imgL.valid(cx,cy) && imgR.valid(cx,cy)){
-                    line[x] = qRgb(qRed(  lineR[cx])*colormult + qGray(lineR[cx])*(1-colormult),
-                                   qGreen(lineL[cx])*colormult + qGray(lineL[cx])*(1-colormult),
-                                   qBlue( lineL[cx])*colormult + qGray(lineL[cx])*(1-colormult));
+                    int gL = qGray(lineL[cx]) * greymult;
+                    int gR = qGray(lineR[cx]) * greymult;
+                    line[x] = qRgb((qRed(  lineL[cx]) * colormult + gL) * redL +
+                                   (qRed(  lineR[cx]) * colormult + gR) * redR,
+                                   (qGreen(lineL[cx]) * colormult + gL) * greenL +
+                                   (qGreen(lineR[cx]) * colormult + gR) * greenR,
+                                   (qBlue( lineL[cx]) * colormult + gL) * blueL +
+                                   (qBlue( lineR[cx]) * colormult + gR) * blueR);
                 }
                 else{
-                    line[x] = qRgb(0,0,0);
+                    line[x] = 0;
                 }
             }
         }
         else{
-            for(int x=0;x<final.width();x++)
-                line[x] = qRgb(0,0,0);
+            for(int x = 0; x < final.width(); x++)
+                line[x] = 0;
         }
     }
 
