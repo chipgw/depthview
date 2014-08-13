1a493eeed477619e40a78c666fac11168ae9c92e
chipgw <gw.chip.gw@gmail.com>
2014-08-12 21:45:44 -0500

    Put path setting code in main after code that doesn't depend on it.

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,6 @@
 int main(int argc, char *argv[]){
     QApplication app(argc, argv);
 
-    if(QDir::currentPath() == QApplication::applicationDirPath()){
-        QDir::setCurrent(QDir::homePath());
-    }
-
     app.setOrganizationName("chipgw");
     app.setApplicationName("DepthView");
     app.setApplicationVersion(version::getVersionString());
@@ -18,6 +14,11 @@ int main(int argc, char *argv[]){
         DepthViewWindow::registerFileTypes();
         return 0;
     }
+
+    if(QDir::currentPath() == app.applicationDirPath()){
+        QDir::setCurrent(QDir::homePath());
+    }
+
     DepthViewWindow window;
     window.show();
     window.parseCommandLine(app.arguments());
