5e1eb1f5d8c39cbfe04a7ebbe3bb5e98afd26c36
chipgw <gw.chip.gw@gmail.com>
2015-07-17 11:27:37 -0500

    Fixed issues with "Save As" & "Export".

--- src/depthviewwindow.cpp
+++ src/depthviewwindow.cpp
@@ -357,22 +357,18 @@ void DepthViewWindow::on_actionSaveAs_triggered(){
 
     if(filename.isNull()) return;
 
-    if(filename.contains(".jps", Qt::CaseInsensitive)){
-        QImage out(ui->imageWidget->imgL.width() + ui->imageWidget->imgR.width(), ui->imageWidget->imgL.height(), QImage::Format_RGB32);
-        QPainter paint(&out);
-        drawSideBySide(ui->imageWidget->pixmapL, ui->imageWidget->pixmapR, 0, 0, paint, false);
+    QImage out(ui->imageWidget->imgL.width() + ui->imageWidget->imgR.width(), ui->imageWidget->imgL.height(), QImage::Format_RGB32);
+    QPainter paint(&out);
 
-        if(!out.isNull()){
+    /* Files are loaded with the left image on the right side, so save stereo images that way too. */
+    drawSideBySide(ui->imageWidget->pixmapR, ui->imageWidget->pixmapL, 0, 0, paint, false);
+
+    if(!out.isNull()){
+        /* We need to tell Qt which format to use, because it can't tell from the extension. */
+        if(filename.contains(".jps", Qt::CaseInsensitive))
             out.save(filename, "JPG");
-        }
-    }else if(filename.contains(".pns", Qt::CaseInsensitive)){
-        QImage out(ui->imageWidget->imgL.width() + ui->imageWidget->imgR.width(), ui->imageWidget->imgL.height(), QImage::Format_RGB32);
-        QPainter paint(&out);
-        drawSideBySide(ui->imageWidget->pixmapL, ui->imageWidget->pixmapR, 0, 0, paint, false);
-
-        if(!out.isNull()){
+        else if(filename.contains(".pns", Qt::CaseInsensitive))
             out.save(filename, "PNG");
-        }
     }
 }
 
@@ -393,35 +389,31 @@ void DepthViewWindow::on_actionExport_triggered() {
     if(dialog.field("anglaph").toBool()){
         QImage out;
 
-        if(dialog.field("fullColor").toBool()){
+        if(dialog.field("fullColor").toBool())
             out = drawAnglaph(ui->imageWidget->imgL, ui->imageWidget->imgR);
-        }else if(dialog.field("halfColor").toBool()){
+        else if(dialog.field("halfColor").toBool())
             out = drawAnglaphHalf(ui->imageWidget->imgL, ui->imageWidget->imgR);
-        }else{
+        else
             out = drawAnglaphGrey(ui->imageWidget->imgL, ui->imageWidget->imgR);
-        }
 
-        if(!out.isNull()){
+        if(!out.isNull())
             out.save(filename, nullptr, quality);
-        }
     }else if(dialog.field("sideBySide").toBool()){
         QImage out(ui->imageWidget->imgL.width() + ui->imageWidget->imgR.width(), ui->imageWidget->imgL.height(), QImage::Format_RGB32);
         QPainter paint(&out);
-        drawSideBySide(ui->imageWidget->pixmapL, ui->imageWidget->pixmapR, 0, 0, paint, 1.0,
+        drawSideBySide(ui->imageWidget->pixmapL, ui->imageWidget->pixmapR, 0, 0, paint, false, 1.0,
                        dialog.field("mirrorL").toBool(), dialog.field("mirrorR").toBool());
 
-        if(!out.isNull()){
+        if(!out.isNull())
             out.save(filename, nullptr, quality);
-        }
     }else if(dialog.field("topBottom").toBool()){
         QImage out(ui->imageWidget->imgL.width(), ui->imageWidget->imgL.height() + ui->imageWidget->imgR.height(), QImage::Format_RGB32);
         QPainter paint(&out);
-        drawTopBottom(ui->imageWidget->pixmapL, ui->imageWidget->pixmapR, 0, 0, paint, 1.0,
+        drawTopBottom(ui->imageWidget->pixmapL, ui->imageWidget->pixmapR, 0, 0, paint, false, 1.0,
                       dialog.field("mirrorT").toBool(), dialog.field("mirrorB").toBool());
 
-        if(!out.isNull()){
+        if(!out.isNull())
             out.save(filename, nullptr, quality);
-        }
     }else if(dialog.field("exportBoth").toBool()){
         QString filenameL = filename;
         ui->imageWidget->imgL.save(filenameL.insert(filenameL.lastIndexOf('.'), 'L'), nullptr, quality);
